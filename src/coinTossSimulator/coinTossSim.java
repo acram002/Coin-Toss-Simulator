04a5649bd3e956f8816bccf53c6f8842e36992b2
Alex Cramer <cramer_alex@yahoo.com>
2022-06-03 15:59:18 -0400

    Prints statistics on results

--- src/coinTossSimulator/coinTossSim.java
+++ src/coinTossSimulator/coinTossSim.java
@@ -10,15 +10,21 @@ import java.util.Scanner;
 
 public class coinTossSim {
 
+	//number of times Heads or Tails
+	public static int h = 0;
+	public static int t = 0;
+	
 	public static void main(String[] args) {
 	
 		Scanner scnr = new Scanner(System.in);
 				//prompts user for # of times to flip coin, then 
-				//calls coinToss w/ # of times
+				//calls coinToss w/ # of times, then
+				//calls stats to print statistics
 		System.out.println("Enter number of times to flip coin: ");
 		int flips = scnr.nextInt();
 		
 		coinToss(flips);
+		stats(flips);
 		
 		scnr.close();
 	}
@@ -36,12 +42,25 @@ public class coinTossSim {
 			//if 1 then heads, if 0 then tails
 				if (result == 1) {
 					System.out.println("Heads!");
+					h++;
 				}
 				else {
 					System.out.println("Tails!");
+					t++;
 				}
 		}
 		
+	}
+	
+	public static void stats(int numTimes) {
+		
+			//prints stats about coin flips
+		System.out.println();
+		System.out.println("Statistics");
+		System.out.println("Number of times Heads: " + h);
+		System.out.println("Number of times Tails: " + t);
+		System.out.println("Percentage Heads: " + (((double) h / (double) numTimes) * 100) + "%");
+		System.out.println("Percentage Tails: " + (((double) t / (double) numTimes) * 100) + "%");
 	}
 
 }
